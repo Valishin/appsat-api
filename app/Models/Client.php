6a302a00cf146277ef9ce65ea7cfdc211e75ad65
Alex ValiDev <abubekirov.alex@gmail.com>
2026-03-05 22:30:26 +0100

    refactor: added cast to type and added values in client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -7,15 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    protected $fillable = [
-        'name',
-        'email',
-        'phone',
-        'dni',
-        'type'
-    ];
-
     protected $casts = [
         'type' => ClientType::class,
     ];
+
+    protected $fillable = [
+        'type',
+        'name',
+        'dni_cif',
+        'phone',
+        'email',
+        'notes'
+    ];
+
 }
