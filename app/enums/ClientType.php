4ea1e918a9e80524ff59aa4665121424150eb37a
Alex ValiDev <abubekirov.alex@gmail.com>
2026-03-05 22:28:43 +0100

    refactor: named type client

--- app/enums/ClientType.php
+++ app/enums/ClientType.php
@@ -4,6 +4,6 @@ namespace App\Enums;
 
 enum ClientType: string
 {
-    case PARTICULAR = 'particular';
-    case PROFESIONAL = 'profesional';
+    case INDIVIDUAL = 'individual';
+    case COMPANY = 'company';
 }
