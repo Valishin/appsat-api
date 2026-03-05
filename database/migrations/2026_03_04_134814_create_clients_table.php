030f09daec7cf7a258dda85cfb6c23cb3be360ea
Alex ValiDev <abubekirov.alex@gmail.com>
2026-03-05 22:30:47 +0100

    fix: migrate tables

--- database/migrations/2026_03_04_134814_create_clients_table.php
+++ database/migrations/2026_03_04_134814_create_clients_table.php
@@ -12,13 +12,29 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('clients', function (Blueprint $table) {
-            $table->id();
+            $table->id();   
+            $table->enum('type', ['individual', 'company']);         
             $table->string('name');
-            $table->string('dni', 20)->unique();
-            $table->string('email')->nullable()->unique();
+            $table->string('dni_cif', 20)->unique();
             $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->text('notes')->nullable();            
 
-            $table->enum('type', ['par', 'profesional']);
+            $table->timestamps();
+        });
+
+        //Address
+        Schema::create('client_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained()->cascadeOnDelete();
+
+            $table->string('address');
+            $table->string('city');
+            $table->string('postal_code', 10)->nullable();
+            $table->string('province')->nullable();
+            $table->string('country')->default('España');
+
+            $table->string('label')->nullable();           
 
             $table->timestamps();
         });
@@ -29,6 +45,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('client_addresses');
         Schema::dropIfExists('clients');
     }
 };
