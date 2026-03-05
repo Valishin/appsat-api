b177bfd30558c47e0e826ee5b68dd9deb951d0d1
Alex ValiDev <abubekirov.alex@gmail.com>
2026-03-05 22:31:28 +0100

    fix: added simple text to front page in laravel

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::get('/', function () {
+    return 'API Laravel funcionando';
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
