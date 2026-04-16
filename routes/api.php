8d81723675391c8f7fb985a59c8b6c5646450e6c
Alex Valishin <abubekirov.alex@gmail.com>
2026-04-16 22:20:12 +0200

    feat: added route get devices by client id

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
     Route::apiResource('clients', ClientController::class);
 
     Route::apiResource('devices', DeviceController::class);
+    Route::get('clients/{clientId}/devices', [DeviceController::class, 'byClient']);
 
     Route::apiResource('client_addresses', AddressController::class);
 
